9d16c6f18a0aa9875f9b85c14452b3d458dd77bc
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2024-08-15 10:04:42 +0200

    ✨ add link to GitHub

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { ThemeProvider } from './components/theme-provider.tsx'
+import { Button } from './components/ui/button.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
       <App />
     </ThemeProvider>
+    <Button variant="link" className="fixed bottom-4 right-4">
+      <a href="https://github.com/HoverBaum/gradientMesh/tree/main">GitHub</a>
+    </Button>
   </StrictMode>
 )
